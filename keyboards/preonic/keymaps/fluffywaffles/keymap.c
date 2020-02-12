affeec22e99181a0cff1cc7c7edbe61b403751d1
fluffywaffles <skorlir@gmail.com>
2020-02-12 18:40:55 -0500

    fluffywaffles: layer[lower]: square brackets under {}

--- keyboards/preonic/keymaps/fluffywaffles/keymap.c
+++ keyboards/preonic/keymaps/fluffywaffles/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Gui  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  \   |
+ * | Gui  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid( \
    KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,        KC_7,        KC_8,     KC_9,     KC_0,      KC_DEL,  \
    KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,        KC_7,        KC_8,     KC_9,     KC_0,     KC_BSLS,  \
-  KC_LGUI,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_UNDS,     KC_PLUS,  KC_LCBR,  KC_RCBR,     KC_BSLS,  \
+  KC_LGUI,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_UNDS,     KC_PLUS,  KC_LBRC,  KC_RBRC,     KC_BSLS,  \
   _______,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  S(KC_NUHS),  S(KC_NUBS),  KC_HOME,   KC_END,     _______,  \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,     KC_MNXT,  KC_VOLD,  KC_VOLU,     KC_MPLY   \
 ),
