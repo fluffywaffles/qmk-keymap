614be22f8d2648c2ef264b21307f55f5caabb7bf
fluffywaffles <skorlir@gmail.com>
2020-02-12 18:31:51 -0500

    fluffywaffles: layer[raise,lower]: replace Del with \; lower \ raise |

--- keyboards/preonic/keymaps/fluffywaffles/keymap.c
+++ keyboards/preonic/keymaps/fluffywaffles/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Esctl|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
    KC_GRV,     KC_1,     KC_2,     KC_3,   KC_4,    KC_5,    KC_6,   KC_7,     KC_8,     KC_9,     KC_0,    KC_BSPC,  \
-   KC_TAB,     KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,    KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,     KC_DEL,  \
+   KC_TAB,     KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,    KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,    KC_BSLS,  \
     ESCTL,     KC_A,     KC_S,     KC_D,   KC_F,    KC_G,    KC_H,   KC_J,     KC_K,     KC_L,  KC_SCLN,    KC_QUOT,  \
   KC_LSFT,     KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,    KC_N,   KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,     KC_ENT,  \
   KC_LCTL,  KC_LGUI,  KC_LCTL,  KC_LALT,  LOWER,  KC_SPC,  KC_SPC,  RAISE,  KC_LEFT,  KC_DOWN,    KC_UP,    KC_RGHT   \
@@ -60,9 +60,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Gui  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * | Gui  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -71,8 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_preonic_grid( \
    KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,        KC_7,        KC_8,     KC_9,     KC_0,     KC_BSPC,  \
-   KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,        KC_7,        KC_8,     KC_9,     KC_0,      KC_DEL,  \
-  KC_LGUI,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_UNDS,     KC_PLUS,  KC_LCBR,  KC_RCBR,     KC_PIPE,  \
+   KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,        KC_7,        KC_8,     KC_9,     KC_0,     KC_BSLS,  \
+  KC_LGUI,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_UNDS,     KC_PLUS,  KC_LCBR,  KC_RCBR,     KC_BSLS,  \
   _______,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  S(KC_NUHS),  S(KC_NUBS),  KC_HOME,   KC_END,     _______,  \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,     KC_MNXT,  KC_VOLD,  KC_VOLU,     KC_MPLY   \
 ),
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   {  |   }  |  |   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Gui  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Gui  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -92,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_TILD,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,     KC_BSPC,  \
-  KC_TILD,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,      KC_DEL,  \
-  KC_LGUI,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,  KC_MINS,   KC_EQL,  KC_LBRC,  KC_RBRC,     KC_BSLS,  \
+  KC_TILD,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,     KC_PIPE,  \
+  KC_LGUI,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,  KC_MINS,   KC_EQL,  KC_LBRC,  KC_RBRC,     KC_PIPE,  \
   _______,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_NUHS,  KC_NUBS,  KC_PGUP,  KC_PGDN,     _______,  \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MNXT,  KC_VOLD,  KC_VOLU,     KC_MPLY   \
 ),
