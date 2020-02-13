dc5afddf24f96e6b6c4fc8eb88cc8dc7e68851c6
fluffywaffles <skorlir@gmail.com>
2020-02-12 20:59:20 -0500

    fluffywaffles: layout[raise]: swap the locations of [] and {}

--- keyboards/preonic/keymaps/fluffywaffles/keymap.c
+++ keyboards/preonic/keymaps/fluffywaffles/keymap.c
@@ -83,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  |   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Gui  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   {  |   }  |  |   |
+ * | Gui  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |   [  |   ]  |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |   {  |   }  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      | PgDn | PgUp |      |
  * `-----------------------------------------------------------------------------------'
@@ -93,8 +93,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_TILD,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LABK,  KC_RABK,     KC_BSPC,  \
   KC_TILD,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,     KC_PIPE,  \
-  KC_LGUI,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,  KC_MINS,   KC_EQL,  KC_LCBR,  KC_RCBR,     KC_PIPE,  \
-  _______,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_NUHS,  KC_NUBS,  KC_LBRC,  KC_RBRC,     _______,  \
+  KC_LGUI,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,  KC_MINS,   KC_EQL,  KC_LBRC,  KC_RBRC,     KC_PIPE,  \
+  _______,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_NUHS,  KC_NUBS,  KC_LCBR,  KC_RCBR,     _______,  \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGDN,  KC_PGUP,     _______   \
 ),
 
