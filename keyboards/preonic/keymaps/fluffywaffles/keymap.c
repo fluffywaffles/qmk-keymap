6fffd8220f6a90897d2d7cacab864594741dd812
fluffywaffles <skorlir@gmail.com>
2020-02-13 13:34:06 -0500

    fluffywaffles: layer[lower,raise]: make Tab LAlt on layers

--- keyboards/preonic/keymaps/fluffywaffles/keymap.c
+++ keyboards/preonic/keymaps/fluffywaffles/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
+ * | Alt  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Gui  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_preonic_grid( \
    KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,        KC_7,        KC_8,     KC_9,     KC_0,      KC_DEL,  \
-   KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,        KC_7,        KC_8,     KC_9,     KC_0,     KC_BSLS,  \
+  KC_LALT,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,        KC_7,        KC_8,     KC_9,     KC_0,     KC_BSLS,  \
   KC_LGUI,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_UNDS,     KC_PLUS,  KC_LBRC,  KC_RBRC,     KC_BSLS,  \
   _______,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  S(KC_NUHS),  S(KC_NUBS),  KC_HOME,   KC_END,     _______,  \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,     KC_MNXT,  KC_VOLD,  KC_VOLU,     KC_MPLY   \
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   <  |   >  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  |   |
+ * | Alt  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  |   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Gui  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_TILD,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LABK,  KC_RABK,     KC_BSPC,  \
-  KC_TILD,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,     KC_PIPE,  \
+  KC_LALT,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,     KC_PIPE,  \
   KC_LGUI,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,  KC_MINS,   KC_EQL,  KC_LBRC,  KC_RBRC,     KC_PIPE,  \
   _______,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_NUHS,  KC_NUBS,  KC_LCBR,  KC_RCBR,     _______,  \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGDN,  KC_PGUP,     _______   \
